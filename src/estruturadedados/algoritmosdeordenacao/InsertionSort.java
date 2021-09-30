baad6a3aff0317b279ad042a55c9262bdf655581
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-09-29 21:36:57 -0300

    Algoritmo Insertion Sort

---
+++ src/estruturadedados/algoritmosdeordenacao/InsertionSort.java
@@ -0,0 +1,45 @@
+package estruturadedados.algoritmosdeordenacao;
+
+public class InsertionSort {
+	//Pior caso: Quando o vetor está invertido
+	//Complexidade quadrática
+	
+	public static void main(String[] args) {
+		int[] vetor =  new int[10000];
+		
+		for(int i = 0; i < vetor.length; i++) {
+			vetor[i] = (int) (Math.random() * vetor.length);
+		}
+		
+//		System.out.println("Vetor desordenado: ");
+//		for(int i = 0; i < vetor.length; i++) {
+//			System.out.println(vetor[i]);
+//		}
+		
+		int aux, j;
+		
+		long inicio =  System.currentTimeMillis();
+		long fim;
+		
+		for(int i = 1; i < vetor.length; i++) {
+			aux = vetor[i];
+			j = i-1;
+			while(j >= 0 && vetor[j] > aux) {
+				vetor[j+1] = vetor[j];
+				j--;
+			}
+			
+			vetor[j+1] = aux;
+		}
+		
+		fim = System.currentTimeMillis();
+		
+//		System.out.println("\n\nVetor ordenado: ");
+//		for(int i = 0; i < vetor.length; i++) {
+//			System.out.println(vetor[i]);
+//		}
+		
+		System.out.println("Tempo: " + (fim - inicio));
+	}
+
+}
