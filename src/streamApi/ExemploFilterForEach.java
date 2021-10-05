524a34651f056af26540480576e8348f078e926d
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-10-05 20:10:12 -0300

    Criando classes Produto e Categoria para 'Stream'

---
+++ src/streamApi/ExemploFilterForEach.java
@@ -0,0 +1,16 @@
+package streamApi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import streamApi.model.Produto;
+
+public class ExemploFilterForEach {
+
+	public static void main(String[] args) {
+		List<Produto> produtos =  new ArrayList<>();
+	
+
+	}
+
+}
