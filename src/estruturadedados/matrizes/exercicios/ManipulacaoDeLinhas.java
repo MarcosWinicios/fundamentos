9c7e352ecb742435757c0e10c5d25bbf93d44f76
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-10-16 00:10:50 -0300

    correção na captura de linha

--- src/estruturadedados/matrizes/exercicios/ManipulacaoDeLinhas.java
+++ src/estruturadedados/matrizes/exercicios/ManipulacaoDeLinhas.java
@@ -8,10 +8,10 @@ public class ManipulacaoDeLinhas {
 	public static void main(String[] args) {
 		int[][] matriz = new int[10][10];
 		matriz = gerarMatriz(matriz);
-		System.out.println("\n\nTamanho da Matriz" + matriz.length);
+		System.out.println("\n\nTamanho da Matriz: " + matriz.length);
 		imprimirMatriz(matriz);
 		
-		System.out.println(Arrays.toString(pegarLinha(matriz, 4)));
+		System.out.println(Arrays.toString(pegarLinha(matriz, 5)));
 
 	}
 	
@@ -26,7 +26,9 @@ public class ManipulacaoDeLinhas {
 	}
 	
 	public static void imprimirMatriz(int[][]matriz) {
+		System.out.println();
 		for(int i = 0; i < matriz.length; i++) {
+			System.out.print("Linha:" + (i + 1) + "| ");
 			for(int j = 0; j < matriz[i].length; j++) {
 				System.out.print((matriz[i][j]) + " ");
 			}
@@ -36,13 +38,14 @@ public class ManipulacaoDeLinhas {
 	}
 	
 	public static int[] pegarLinha(int[][] matriz, int linha) {
-//		linha = linha -1;
 		
-		int[] valoresDaLinha = new int[matriz.length];
+		System.out.println("Linha: " + linha);
+		int tamanho = matriz.length;
+		int[] valoresDaLinha = new int[tamanho];
 		
 		for(int i = 0; i < matriz.length; i++) {
 			for(int j = 0; j < matriz[i].length; j++) {
-				if(i == linha) {
+				if(i == linha - 1 ) {
 					valoresDaLinha[j] = matriz[i][j];
 				}
 			}
@@ -50,7 +53,4 @@ public class ManipulacaoDeLinhas {
 		}
 		return valoresDaLinha;
 	}
-	
-	
-
 }
