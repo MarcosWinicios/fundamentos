db55d46e25d9d400cee2795260135ed3951a2a72
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2023-12-17 21:20:21 -0300

    [DESAFIO] Adicionando comentários para gerar documentação javadoc no desafio das Collections

--- src/desafios/gastosDePoliticos/Governo.java
+++ src/desafios/gastosDePoliticos/Governo.java
@@ -6,15 +6,29 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+/**
+ * Classe que representa a organização de políticos de um Governo
+ * @author Marcos Winicios
+ */
 public class Governo {
 	// armazena uma lista de políticos por estado da federação
+	
+	/**
+	 * Atributo da classe Governo que utiliza o a inteface Map para armazenar os partidos políticos do Governo
+	 * 
+	 */
 	private Map<String, List<Politico>> partidosPoliticos;
 
 	public Governo() {
 		this.partidosPoliticos = new HashMap<String, List<Politico>>();
 	}
-
+	
+	/**
+	 * Método utilizado para adicionar um político a um partido.
+	 * 
+	 * @param partidoPolitico Objeto do tipo String que representa o nome do partido politico
+	 * @param politico Objeto do tipo Politico que representa um politico
+	 */
 	public void adicionarPolitico(String partidoPolitico, Politico politico) {
 		// recupera a lista de políticos para um partido
 		List<Politico> politicos = this.partidosPoliticos.get(partidoPolitico);
@@ -32,7 +46,13 @@ public class Governo {
 		// como chave o nome do partido
 		this.partidosPoliticos.put(partidoPolitico, politicos);
 	}
-
+	
+	/**
+	 * Método capaz de calcular os salários dos políticos de determinado partido.
+	 * 
+	 * @param partidoPolitico Objeto do tipo String que representa o nome de um partido político.
+	 * @return Retorna um objeto do tipo BigDecimal que representa o cálculo total dos gastos com os salários de políticos do partido recebido nos parâmetros.
+	 */
 	public BigDecimal calcularGastosComSalario(String partidoPolitico) {
 		// implementar busca de políticos para o partido informado
 		// e cálculo dos salários
@@ -48,6 +68,13 @@ public class Governo {
 		return resultado;
 	}
 
+	/**
+	 * Método capaz de calcular os gastos totais com políticos de um determinado cargo de um determinado partido
+	 * 
+	 * @param cargo Objeto do tipo Cargo que representa qual categoria de cargos de políticos será considerada para o cálculo
+	 * @param partidoPolitico Objeto do tipo String que representa o partido político utilizado para o cálculo
+	 * @return Retorna um Objeto do tipo BigDecimal que representa os gastos totais com salários de políticos.
+	 */
 	public BigDecimal calcularGastosComSalarioParaCargo(Cargo cargo, String partidoPolitico) {
 		// implementar busca dos políticos para o partido e cargo informados
 		// e cálculo dos salários
@@ -84,7 +111,11 @@ public class Governo {
 		resultado.append(this.quebraDeLinha(" # "));
 		return resultado.toString();
 	}
-
+	
+	/**
+	 * Método utilizado para gerar um relatório com detalhes de todos os políticos de cada partido do Governo
+	 * @return retorna um Objeto do tipo String contendo as informações do relatório
+	 */
 	public String obterRelatorioGoverno() {
 		StringBuilder resultado = new StringBuilder();
 		resultado.append(this.quebraDeLinha(" = "));
