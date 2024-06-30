367b7993d4d462f6ee0bda803179dea1b92d83b0
Marcos Winicios Pereira Martins <mwmartins1@topaz.com.br>
2024-06-29 21:24:39 -0300

    feat: Streams API Final Operations

--- src/lambda/Java8Streams.java
+++ src/lambda/Java8Streams.java
@@ -1,15 +1,16 @@
 package lambda;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Java8Streams {
 
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 5, 8, 9, 1, 4, 7, 6, 6, 9, 9);
 
-        System.out.println("Operações intermediárias: \n");
         //strem =  fluxo de dados
+
+        System.out.println("Operações intermediárias: \n");
         // Filtrando dados
         list.stream()
                 .skip(2) //ignorar elementos
@@ -20,5 +21,21 @@ public class Java8Streams {
                 .forEach(e -> System.out.print(e + " "));
 
         System.out.println( "\n" + list);
+
+        System.out.println("\nOperações finais: \n");
+
+        String collect = list.stream()
+//                .filter(e -> e % 2 == 0)
+//                .map(e -> e * 3)
+                .map(e -> String.valueOf(e))
+//                .count(); // retorna quantidade de elementos
+//                .min(Comparator.naturalOrder()); // Min e Max - Retorna o maior ou menor valor da lista. É preciso passar um comparador
+//                .collect(Collectors.toList()); //Coleta todos os elementos e devolve em uma nova lista
+//                .collect(Collectors.groupingBy(e -> e % 2 == 0)); // Gera um mapa agrupando os elemtnos pelo filtro personalizado (par e impar)
+//                .collect(Collectors.groupingBy(e -> e % 3));// Gera um mapa agrupando os elemtnos pelo filtro personalizado (resto da divisão por 3)
+                .collect(Collectors.joining(";")); // Concatena todos os elementos em uma única String (Só trabalha com Strings)
+
+        System.out.println(collect);
+
     }
 }
