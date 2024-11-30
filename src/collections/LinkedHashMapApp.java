0773268d555c5c30fb0d5213071e42267d098f4d
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-11-29 23:48:27 -0300

    feat(collections): add LinkedHashMap implementation

---
+++ src/collections/LinkedHashMapApp.java
@@ -0,0 +1,19 @@
+package collections;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class LinkedHashMapApp {
+
+    public static void main(String[] args) {
+        LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
+
+        map.put(4, "Bruno");
+        map.put(2, "Umberto");
+        map.put(3, "Fernando");
+        map.put(3, "Caetano");
+        map.put(1, "Umberto");
+
+        System.out.println(map);
+    }
+}
